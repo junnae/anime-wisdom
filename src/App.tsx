118d3c05e256c882a8232a566dab6517667d395b
Aleksander Andresen <caeses@gmail.com>
2021-04-01 21:47:54 +0200

    Credit giphy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './style/App.scss'
-import Header from "./Components/Header";
+import Header from "./Components/layout/Header";
 import MainContent from "./Components/content/MainContent";
+import Footer from "./Components/layout/Footer";
 
 function App() {
     return (
         <div className="App">
         <Header/>
         <MainContent/>
+        <Footer/>
         </div>
     );
 }
---
+++ src/Components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import '../../style/Footer.scss'
+import React from "react";
+
+const Footer: React.FC = () => {
+        return (
+            <div className={"App-footer"}>
+                <img src={"giphy.gif"} alt={"Powered by giphy"}/>
+            </div>
+        )
+}
+
+export default Footer
