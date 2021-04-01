176c7c007713a0f535b2c7e4597a193588bc258f
Aleksander Andresen <caeses@gmail.com>
2021-04-01 11:14:55 +0200

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // @ts-ignore
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
