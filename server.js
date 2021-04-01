a9d697a4a25c1f845fe72ae2e1545ffe8a6fd081
Aleksander Andresen <caeses@gmail.com>
2021-04-01 11:11:08 +0200

    Read from env file locally

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const axios = require("axios");
 const app = express();
+require('dotenv').config()
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/animegif', function (req, res) {
