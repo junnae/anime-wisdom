f17bcda44d24274779b559b4a1cf10cb70e325db
Aleksander Andresen <caeses@gmail.com>
2021-04-04 01:28:57 +0200

    try something else

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/*/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 
