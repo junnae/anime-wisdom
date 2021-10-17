e4200f5c19ac8c68f64512442a2eb9a5c169e4c7
Aleksander Andresen <caeses@gmail.com>
2021-10-17 15:38:30 +0200

    Stop using database to save money 💸

--- server.js
+++ server.js
@@ -1,40 +1,30 @@
 const express = require('express');
 const {fetchRandomAnimeGif, fetchQuote} = require("./server/Integration");
-const {QuoteCache} = require('./server/QuoteCache');
-const {GifCache} = require('./server/GifCache');
+const {Cache} = require('./server/Cache');
 
 const path = require('path');
-const {connect, getQuoteById} = require("./server/Database");
 const app = express();
 require('dotenv').config()
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-let gifCache = new GifCache(1000, true)
-let quoteCache = new QuoteCache(1000, false)
+let gifCache = new Cache(1000, true)
+let quoteCache = new Cache(1000, false)
 
 //TODO - Basic Error Handling
 app.get('/animegif', function (req, res) {
     let queryId = req.query.id;
-    gifCache.maybeGetFromCache(queryId, function(gifFromCache){
-        if (gifFromCache !== undefined) return res.send(gifFromCache)
-        else return fetchRandomAnimeGif(gifCache, res, queryId === undefined)
-    });
+    let fromCache = gifCache.maybeGetFromCache(queryId);
+    if (fromCache !== undefined)
+        return res.send(fromCache)
+    return fetchRandomAnimeGif(gifCache, res, queryId === undefined)
 });
 
 app.get('/advice', function (req, res) {
-    let queryId = req.query.id;
-    if (queryId !== undefined) {
-        getQuoteById(queryId, function (err, result) {
-            if (err) console.error(err)
-            else if (result !== undefined) return res.send(result)
-        });
-    } else {
-        quoteCache.maybeGetFromCache(undefined, function (quoteFromCache) {
-            if (quoteFromCache !== undefined) return res.send(JSON.stringify(quoteFromCache))
-            else fetchQuote(quoteCache, res);
-        });
-    }
+    let fromCache = quoteCache.maybeGetFromCache();
+    if (fromCache !== undefined)
+        return res.send(fromCache)
+    return fetchQuote(quoteCache, res);
 });
 
 
@@ -53,5 +43,4 @@ app.get('/ping', function (req, res) {
 
 app.listen(8080, function () {
     console.log('Server started')
-    connect()
 });
